4c122a9d460b106a62acd28242d1b3bbf1520fdc
Thomas Citharel <tcit@tcit.fr>
2016-02-07 16:52:59 +0100

    comment work

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -34,6 +34,7 @@ class AppKernel extends Kernel
             new Wallabag\ImportBundle\WallabagImportBundle(),
             new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle(),
             new Craue\ConfigBundle\CraueConfigBundle(),
+            new Wallabag\CommentBundle\WallabagCommentBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
